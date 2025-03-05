81da774b872d860348aac2509b65d6ba4a6e8431
julesrb <jbjulesbernard3@gmail.com>
2025-03-05 12:55:22 +0100

    init

---
+++ s00_ex00/s00_ex00/s00_ex00App.swift
@@ -0,0 +1,17 @@
+//
+//  s00_ex00App.swift
+//  s00_ex00
+//
+//  Created by Jules BERNARD on 05.03.25.
+//
+
+import SwiftUI
+
+@main
+struct s00_ex00App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
