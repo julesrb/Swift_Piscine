e7ea978024baf48886a10b46578437527b426bcb
Jules <jbjulesbernard3@gmail.com>
2025-10-15 18:13:36 +0200

    stronger .gitignore

---
+++ ex00-2/ex78888/ex78888/ex78888App.swift
@@ -0,0 +1,17 @@
+//
+//  ex78888App.swift
+//  ex78888
+//
+//  Created by jules bernard on 15.10.25.
+//
+
+import SwiftUI
+
+@main
+struct ex78888App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ex00-2/ex78888/ex78888Tests/ex78888Tests.swift
@@ -0,0 +1,16 @@
+//
+//  ex78888Tests.swift
+//  ex78888Tests
+//
+//  Created by jules bernard on 15.10.25.
+//
+
+import Testing
+
+struct ex78888Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
